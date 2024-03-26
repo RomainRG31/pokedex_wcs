12532554f1c08cd31dc6d772070f77c46f91b105
Romain <97295039+RomainRG31@users.noreply.github.com>
2024-03-26 12:08:02 +0100

    add bulbasaur image and create PokemonCard component

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function PokemonCard() {
+  return (
+    <>
+      <figure>
+        <img
+          src="./src/assets/img/bubli.png"
+          alt='Image du Pokémon "Bulbasaur".'
+        />
+      </figure>
+      <figcaption>Bulbasaur</figcaption>
+    </>
+  );
+}
+
+export default PokemonCard;
